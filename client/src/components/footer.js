02b7aa6b5f14ee541e6bf02fac0d2c3869a44143
Toni4ALG <82338185+Toni4ALG@users.noreply.github.com>
2021-04-25 03:38:33 +0100

    Update footer.js

--- client/src/components/footer.js
+++ client/src/components/footer.js
@@ -5,7 +5,7 @@ function Footer() {
     return (
 <footer className="footer">
       <div className="container text-center">
-        <span style={{color:"grey"}}>By <button style={{display: "contents", color:'white'}}>@Dilawfird</button></span>
+        <span style={{color:"grey"}}>By <button style={{display: "contents", color:'white'}}>@Toni_ALG</button></span>
         
       </div>
     </footer>
